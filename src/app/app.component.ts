5a9fe8da4810def22abb0b6582f4760524b6dbb4
Ken Sodemann <ken@ionic.io>
2023-01-13 11:01:24 -0600

    add the explore container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'test-bind';
+  appName = 'This is the Foo Bar';
+  appAltName = 'It is full of Baz';
 }
